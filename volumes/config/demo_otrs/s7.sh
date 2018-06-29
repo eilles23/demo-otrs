2ad8db307cf0de8b78f983b1c00f39bdae356457
eilles23 <eilles23@github.com>
2018-06-29 21:44:06 +0200

    fin

--- volumes/config/demo_otrs/s7.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-sudo docker exec demootrs_mailclient rm -rf /home/developer/.sylpheed-2.0/
-sudo docker cp /opt/otrs/Kernel/demo_otrs/s5/mailbox/ demootrs_mailclient:/home/developer/.sylpheed-2.0/
-
